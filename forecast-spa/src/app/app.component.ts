9ca79576cc97621537c8dfa386efc34cdd40f160
killer <michael.molchanov.2004@gmail.com>
2024-05-16 18:29:58 +0200

    angular material

--- forecast-spa/src/app/app.component.ts
+++ forecast-spa/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatButtonToggle } from '@angular/material/button-toggle';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, MatButtonToggle],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
