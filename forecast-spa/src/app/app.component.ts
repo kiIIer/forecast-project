abd14d524e92673b59caeb0be16e8e6921b5ac3d
killer <michael.molchanov.2004@gmail.com>
2024-05-17 15:16:45 +0200

    cities service

--- forecast-spa/src/app/app.component.ts
+++ forecast-spa/src/app/app.component.ts
@@ -3,22 +3,32 @@ import { RouterModule } from '@angular/router';
 import { MatButtonToggle } from '@angular/material/button-toggle';
 import { MatButton } from '@angular/material/button';
 import { AuthService } from '@auth0/auth0-angular';
-import { isPlatformBrowser } from '@angular/common';
+import { AsyncPipe, isPlatformBrowser, JsonPipe, NgForOf, NgIf } from '@angular/common';
+import { CityService } from './services/city.service';
+import { Observable } from 'rxjs';
+import { City } from './store/city/city.model';
+import { MatLine } from '@angular/material/core';
+import { MatList, MatListItem } from '@angular/material/list';
 
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatButtonToggle, MatButton],
+  imports: [RouterModule, MatButtonToggle, MatButton, AsyncPipe, MatLine, MatListItem, MatList, NgForOf, JsonPipe, NgIf],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'forecast-spa';
+  testCity = {
+    name: 'Test City 3'
+  } as City;
 
   auth: AuthService | null = null;
+  cities$: Observable<City[]> | null = null;
+  postCity$: Observable<City> | null = null;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: NonNullable<unknown>) {
+  constructor(@Inject(PLATFORM_ID) private platformId: NonNullable<unknown>, public cityService: CityService) {
     if (isPlatformBrowser(this.platformId)) {
       // Conditionally inject AuthService only on the client side
       this.auth = inject(AuthService);
--- forecast-spa/src/app/app.config.ts
+++ forecast-spa/src/app/app.config.ts
@@ -9,6 +9,7 @@ import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideRouterStore } from '@ngrx/router-store';
 import { reducers } from './store';
 import { provideAuth0 } from '@auth0/auth0-angular';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideEffects(),
     provideStore(reducers),
     provideStoreDevtools(),
+    provideHttpClient(),
     provideRouterStore(),
     provideClientHydration(),
     provideAnimationsAsync()
