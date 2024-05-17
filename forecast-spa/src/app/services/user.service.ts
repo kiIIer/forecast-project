eb907fc138f5252c006e5a3522bef1e48a513e35
killer <michael.molchanov.2004@gmail.com>
2024-05-17 15:31:33 +0200

    user service

---
+++ forecast-spa/src/app/services/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { City } from '../store/city/city.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  endpoint = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {
+  }
+
+  loadNotifications(): Observable<Notification[]> {
+    return this.http.get<Notification[]>(`${this.endpoint}/upcoming-forecasts`);
+  }
+
+  loadFavorites(): Observable<City[]> {
+    return this.http.get<City[]>(`${this.endpoint}/favorites`);
+  }
+
+  postFavorite(city: City): Observable<City> {
+    return this.http.post<City>(`${this.endpoint}/favorites`, city);
+  }
+
+  deleteFavorite(id: string): Observable<City> {
+    return this.http.delete<City>(`${this.endpoint}/favorites/${id}`);
+  }
+}
