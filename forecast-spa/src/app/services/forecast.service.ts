c002b9a411db748f07b3360d80d6895ae46c0869
killer <michael.molchanov.2004@gmail.com>
2024-05-17 15:28:12 +0200

    forecasts service

---
+++ forecast-spa/src/app/services/forecast.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Forecast } from '../store/forecast/forecast.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ForecastService {
+  endpoint = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  loadForecasts(cityId: string): Observable<Forecast[]> {
+    return this.http.get<Forecast[]>(`${this.endpoint}/forecasts`);
+  }
+
+  postForecast(forecast: Forecast): Observable<Forecast> {
+    return this.http.post<Forecast>(`${this.endpoint}/forecasts`, forecast);
+  }
+
+  deleteForecast(id: string): Observable<Forecast> {
+    return this.http.delete<Forecast>(`${this.endpoint}/forecasts/${id}`);
+  }
+}
