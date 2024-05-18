ff38645d0f308a68797c191eddb2e6586122f491
killer <michael.molchanov.2004@gmail.com>
2024-05-18 16:07:03 +0200

    i think the guard is working

--- forecast-spa/src/app/guards/cities.guard.ts
+++ forecast-spa/src/app/guards/cities.guard.ts
@@ -1,14 +1,19 @@
 import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { catchError, filter, map, Observable, of, scheduled, switchMap, tap } from 'rxjs';
 import { City } from '../store/city/city.model';
 import { CityActions } from '../store/city/city.actions';
 import { selectAll } from '../store/city/city.reducer';
+import { isPlatformBrowser } from '@angular/common';
 
 export const citiesGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   const store = inject(Store);
+  const platformId = inject(PLATFORM_ID);
 
+  if (!isPlatformBrowser(platformId)) {
+    return of(false);
+  }
 
   const checkStore = (): Observable<boolean> => {
     return store.select(selectAll).pipe(
